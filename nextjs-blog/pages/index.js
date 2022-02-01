00d7e45132bdcf8bd6ebf89fbaea3a1e9c97913a
Istomina Alena <mynikisalena@gmail.com>
2022-02-01 04:58:18 +0200

    Add basic settings to the App

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,15 @@
+import CustomHead from "../components/CustomHead";
+import SectionWrapper from "../shared/components/SectionWrapper";
+import OurClients from "../components/OurClients";
+
+const Home = () => {
+  return (
+    <>
+      <CustomHead />
+      {/* <SectionWrapper title="myApp"></SectionWrapper> */}
+      <OurClients />
+    </>
+  );
+};
+
+export default Home;
